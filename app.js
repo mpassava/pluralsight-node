e0f7abd877e4c57afbafd063faabcaf24317c451
mpassava <mpassava@gmail.com>
2024-09-27 01:01:21 -0400

    added sessions router and pages

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ import express from "express";
 import chalk from "chalk";
 import debugModule from "debug";
 import morgan from "morgan";
+import sessionsRouter from "./src/routers/sessionsRouter.js";
 
 const PORT = process.env.PORT || 3000;
 const debug = debugModule("app");
 const app = express();
-const sessionRouter = express.Router();
 
 app.use(morgan("tiny"));
 app.use(express.static(`${import.meta.dirname}/public/`));
@@ -14,22 +14,7 @@ app.use(express.static(`${import.meta.dirname}/public/`));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-sessionRouter.route("/").get((req, res) => {
-  res.render("sessions", {
-    sessions: [
-      { title: "Session 1", description: "This is session 1" },
-      { title: "Session 2", description: "This is session 2" },
-      { title: "Session 3", description: "This is session 3" },
-      { title: "Session 4", description: "This is session 4" },
-    ],
-  });
-});
-
-sessionRouter.route("/1").get((req, res) => {
-  res.send("hello single session");
-});
-
-app.use("/sessions", sessionRouter);
+app.use("/sessions", sessionsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Globomantics", data: ["a", "b", "c"] });
---
+++ src/routers/sessionsRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import sessions from '../data/sessions.json' with { type: 'json' };
+
+const sessionsRouter = express.Router();
+
+sessionsRouter.route("/").get((req, res) => {
+  res.render("sessions", { sessions });
+});
+
+sessionsRouter.route("/:id").get((req, res) => {
+  const id = req.params.id;
+  res.render('session', {
+    session: sessions[id]
+  });
+});
+
+export default sessionsRouter;
