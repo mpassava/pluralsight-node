edba4c0464c306b6781753cfa80ec7a2e65be8cc
mpassava <mpassava@gmail.com>
2024-09-26 22:48:27 -0400

    added view engine

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-import express from 'express';
-import chalk from 'chalk';
-import debugModule from 'debug';
-import morgan from 'morgan';
+import express from "express";
+import chalk from "chalk";
+import debugModule from "debug";
+import morgan from "morgan";
 
-const debug = debugModule('app');
+const PORT = process.env.PORT || 3000;
+const debug = debugModule("app");
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(express.static(`${import.meta.dirname}/public/`));
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Globomantics" });
 });
 
-app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(PORT, () => {
+  debug(`listening on port ${chalk.green(PORT)}`);
 });
